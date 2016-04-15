bec69c030e0cb56686cd153f9631252dea8a9a6a
freightrunner <arpaio@gmail.com>
2016-04-15 14:31:01 -0400

    still working on login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			flash[:success] = "Successfully logged in"
+			redirect_to users_path(user)
+		else
+			flash.now[:danger] = "Wrong email/password"
+			render 'new'
+		end
 	end
 
 	def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	validates :username, presence: true,
 						uniqueness: { case_sensitive: false },
-						length: { minimum: 3, maximum: 10 }
+						length: { minimum: 3, maximum: 15 }
 
 	VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true,
